e80ec1960f61b09ba8260d5b2e6b6fba4e9d914a
diegobenevidss <diegobfontenele@gmail.com>
2026-05-27 11:36:43 -0300

    fix(api): use dynamic API URL in Vercel proxy

--- api/[...path].js
+++ api/[...path].js
@@ -1,10 +1,10 @@
 export default async function handler(req, res) {
   const { path } = req.query;
   const targetPath = Array.isArray(path) ? path.join('/') : (path || '');
-
-  const backendBaseUrl = process.env.BACKEND_API_BASE_URL || 'http://20.206.194.184';
-  const targetUrl = `${backendBaseUrl.replace(/\/$/, '')}/api/${targetPath}`;
-
+  
+  const baseUrl = process.env.BACKEND_API_BASE_URL || process.env.VITE_API_BASE_URL || 'http://api.20.206.214.132.nip.io';
+  const targetUrl = `${baseUrl.replace(/\/$/, '')}/api/${targetPath}`;
+  
   const headers = { ...req.headers };
   delete headers.host;
   delete headers.origin;
